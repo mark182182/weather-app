5fa8008f3433222c0200dbc5b62b1d3f6d2839b7
mark182182 <versalife11@hotmail.com>
2019-12-23 18:34:14 +0100

    fix(urls): use secure geonames API

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,3 +1,3 @@
-export const GEO_NAMES = 'http://api.geonames.org/findNearbyPlaceNameJSON',
+export const GEO_NAMES = 'https://secure.geonames.org/findNearbyPlaceNameJSON',
   WEATHER_MAP = 'https://api.openweathermap.org/data/2.5/weather',
   GEOCAGE = 'https://api.opencagedata.com/geocode/v1/json';
