a83e7f4722326e1aa86c653cbebf1af03f714a0d
mark182182 <versalife11@hotmail.com>
2019-12-31 14:58:01 +0100

    test(geolocation): stripWeatherMap, mergePropValue, stripLatLong functions

--- src/reducer/geolocation.js
+++ src/reducer/geolocation.js
@@ -1,12 +1,15 @@
-import { getGeoNames, getWeatherMap, getLatLongByName } from '../services/requests';
+import {
+  getGeoNames,
+  getWeatherMap,
+  getLatLongByName,
+} from '../services/requests';
 
 export const stripGeoNames = async geoProps => {
   const geoNames = await getGeoNames(geoProps);
-  return geoNames.geonames
-    .map(loc => {
-      return { lat: loc.lat, lng: loc.lng, name: loc.name };
-    });
-}
+  return geoNames.geonames.map(loc => {
+    return { lat: loc.lat, lng: loc.lng, name: loc.name };
+  });
+};
 
 export const loadWeatherForLocation = async geoProps => {
   let weather = [];
@@ -14,20 +17,20 @@ export const loadWeatherForLocation = async geoProps => {
     weather = [...weather, await stripWeatherMap(geoProps[location])];
   }
   return weather;
-}
+};
 
 export const stripWeatherMap = async geoProps => {
   const weatherMap = await getWeatherMap(geoProps);
   const { weather, main, name } = mergePropValue(weatherMap, geoProps.name);
   return { weather, main, name };
-}
+};
 
 export const mergePropValue = (objectToAdd, propValue) => {
   objectToAdd.name = propValue;
   return objectToAdd;
-}
+};
 
 export const stripLatLong = async geoProps => {
   const strippedLatLong = await getLatLongByName(geoProps);
   return strippedLatLong.results;
-}
+};
--- src/reducer/geolocation.test.js
+++ src/reducer/geolocation.test.js
@@ -5,16 +5,6 @@ import {
   mergePropValue,
   stripLatLong,
 } from './geolocation';
-import { getGeoNames } from '../services/geolocation';
-
-it('should call stripGeoNames', () => {
-  const mock = {
-    stripGeoNames: jest.fn(),
-    getGeoNames: jest.fn(),
-  };
-  mock.stripGeoNames();
-  expect(mock.stripGeoNames).toHaveBeenCalled();
-});
 
 it('should return the stripped geolocation data ', async () => {
   const props = {
@@ -30,4 +20,83 @@ it('should return the stripped geolocation data ', async () => {
       name: expect.any(String),
     },
   ]);
+});
+
+it('should return the weather data for the given location', async () => {
+  const props = {
+    lat: 49.5139086,
+    lng: 18.8602198,
+    name: 'Rainbow City',
+  };
+  const response = {
+    weather: [
+      {
+        description: expect.any(String),
+        icon: expect.any(String),
+        id: expect.any(Number),
+        main: expect.any(String),
+      },
+    ],
+    main: {
+      temp: expect.any(Number),
+      feels_like: expect.any(Number),
+      humidity: expect.any(Number),
+      pressure: expect.any(Number),
+      temp: expect.any(Number),
+      temp_max: expect.any(Number),
+      temp_min: expect.any(Number),
+    },
+    name: 'Rainbow City',
+  };
+  expect(await loadWeatherForLocation([props])).toStrictEqual([response]);
+});
+
+it('should the stripped weather data', async () => {
+  const props = {
+    lat: 49.5139086,
+    lng: 18.8602198,
+    name: 'Rainbow City',
+  };
+  const response = {
+    weather: [
+      {
+        description: expect.any(String),
+        icon: expect.any(String),
+        id: expect.any(Number),
+        main: expect.any(String),
+      },
+    ],
+    main: {
+      temp: expect.any(Number),
+      feels_like: expect.any(Number),
+      humidity: expect.any(Number),
+      pressure: expect.any(Number),
+      temp: expect.any(Number),
+      temp_max: expect.any(Number),
+      temp_min: expect.any(Number),
+    },
+    name: 'Rainbow City',
+  };
+  expect(await stripWeatherMap(props)).toStrictEqual(response);
+});
+
+it('should create a name property with the given value', () => {
+  const obj = {
+    weather: 'cold',
+  };
+  const response = {
+    weather: 'cold',
+    name: 'Rainbow City',
+  };
+  mergePropValue(obj, 'Rainbow City');
+  expect(obj).toStrictEqual(response);
+});
+
+it('should return the given location data based on input', async () => {
+  const response = {
+    lat: expect.any(Number),
+    lng: expect.any(Number),
+  };
+  const asd = await stripLatLong({ name: 'London' });
+  expect(asd[0].geometry).toStrictEqual(response);
 });
